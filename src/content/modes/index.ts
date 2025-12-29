3ad9c8ec99f4283588fe81afc101b0f75d2a2532
sandeepsingh61935 <sandeepss128961@gmail.com>
2025-12-29 17:30:41 +0000

    chore(core): export WalkMode from modes module

--- src/content/modes/index.ts
+++ src/content/modes/index.ts
@@ -5,4 +5,5 @@
 export type { IHighlightMode, HighlightData } from './highlight-mode.interface';
 export { BaseHighlightMode } from './base-highlight-mode';
 export { SprintMode } from './sprint-mode';
+export { WalkMode } from './walk-mode';
 export { ModeManager } from './mode-manager';
